c1d5514e3b0d3dbe063ffe94086844e45af62ab9
Nakata Nobuyuki <nobu.nakata@gmail.com>
2016-01-31 18:51:42 +0900

    no message

--- Assets/Packages/PolygonalLightVolume/LightCapture.cs
+++ Assets/Packages/PolygonalLightVolume/LightCapture.cs
@@ -8,38 +8,39 @@ namespace PolygonalLightVolume {
 		public const string PROP_SCALE = "_Scale";
 
 		public LayerMask lightVolumeLayer = 1 << 12;
-		public Camera referenceCam;
 		public Shader debugShader;
 		public float scale = 1f;
 
+		Camera _mainCam;
 		Camera _lightCam;
 		Material _debugMat;
 
 		void Start() {
-			_lightCam = GetComponent<Camera> ();
+			_mainCam = GetComponent<Camera> ();
+
+			var lightCamObj = new GameObject ("Light Cam", typeof(Camera));
+			_lightCam = lightCamObj.GetComponent<Camera> ();
 
 			_debugMat = new Material (debugShader);
 		}
 		void OnDestroy() {
+			Destroy (_lightCam.gameObject);
 			Destroy (_debugMat);
 		}
-		void Update() {
-			_lightCam.transform.SetParent (referenceCam.transform, false);
+		void OnPreRender() {
+			_lightCam.transform.SetParent (_mainCam.transform, false);
 			_lightCam.transform.localPosition = Vector3.zero;
 			_lightCam.transform.localRotation = Quaternion.identity;
 			_lightCam.transform.localScale = Vector3.one;
 
-			_lightCam.CopyFrom (referenceCam);
+			_lightCam.CopyFrom (_mainCam);
 			_lightCam.depth++;
 			_lightCam.cullingMask = lightVolumeLayer;
-			_lightCam.clearFlags = CameraClearFlags.SolidColor;
+			_lightCam.clearFlags = CameraClearFlags.Nothing;
 			_lightCam.backgroundColor = Color.black;
-			_lightCam.depthTextureMode |= DepthTextureMode.Depth;
+			_lightCam.depthTextureMode = DepthTextureMode.Depth;
 
 			_debugMat.SetFloat (PROP_SCALE, scale);
-		}
-		void OnRenderImage(RenderTexture src, RenderTexture dst) {
-			Graphics.Blit (src, dst, _debugMat);
 		}
 	}
 }
